2e3b0f4321a8c5428c1e3365d3f030599decbb95
hypuyonghua <hypuyonghua@163.com>
2018-05-16 17:26:58 +0800

    刷新内容w

--- src/main/java/com/ms/phy/Phytest.java
+++ src/main/java/com/ms/phy/Phytest.java
@@ -4,5 +4,6 @@ public class Phytest {
     public static void main(String[] args) {
         System.out.println("bdfkjkgjdi");
         System.out.println("刷新内容");
+        System.out.println("从新提交");
     }
 }
